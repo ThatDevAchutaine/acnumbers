9664fc36532bd732bc79145fde7753ce23d7444a
ThatDevAchutaine <86481246+ThatDevAchutaine@users.noreply.github.com>
2021-07-13 00:08:01 +0100

    Updated index.js
    Added a TypeError

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const chalk = require('chalk');
 
 module.exports = {
     addOne: async (number) => {
+        if (!number) throw new TypeError("No number was found to add on to!?")
+
         console.log(chalk.red(`${number += 1}`));
         return number += 1;
     }
